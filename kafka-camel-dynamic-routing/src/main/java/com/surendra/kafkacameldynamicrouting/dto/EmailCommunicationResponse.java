77001f4d0dcdcd317b005dc1d74cfdbbc54becae
Surendra Bajracharya <bajracharya.surendra@gmail.com>
2021-03-11 14:00:51 +0000

    kafka-camel-dynamic-routing - WIP

--- kafka-camel-dynamic-routing/src/main/java/com/surendra/kafkacameldynamicrouting/dto/EmailCommunicationResponse.java
+++ kafka-camel-dynamic-routing/src/main/java/com/surendra/kafkacameldynamicrouting/dto/EmailCommunicationResponse.java
@@ -1,2 +1,20 @@
-package com.surendra.kafkacameldynamicrouting.dto;public class EmailCommunicationResponse {
+package com.surendra.kafkacameldynamicrouting.dto;
+
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement(name = "EmailCommunicationResponse")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {"acknowledgementResponse"})
+public class EmailCommunicationResponse {
+
+    @XmlElement(name="acknowledgementResponse", required = true)
+    protected Acknowledgement acknowledgementResponse;
+
+    public Acknowledgement getAcknowledgementResponse() {
+        return acknowledgementResponse;
+    }
+
+    public void setAcknowledgementResponse(Acknowledgement acknowledgementResponse) {
+        this.acknowledgementResponse = acknowledgementResponse;
+    }
 }
